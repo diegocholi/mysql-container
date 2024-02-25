c29ee1965ad7bec82f34bdd2e298f0514b564ff8
Diego Choli Conceição <48794883+diegocholi@users.noreply.github.com>
2024-02-25 11:21:58 -0300

    Update database.sh

--- database.sh
+++ database.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
-
+# Restaura Banco de Dados
 # Tentativa de conexão com o MySQL a cada 5 segundos, até 10 tentativas.
 tentativa=1
-while ! mysql -h "localhost" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SELECT 1" > /dev/null 2>&1; do
-    echo "Tentando conectar ao MySQL: Tentativa $tentativa"
+while ! mysql -h $DB_HOST -u "$DB_USERNAME" -p"$DB_PASSWORD" -e "SELECT 1" > /dev/null 2>&1; do
     sleep 5
+    echo "Tentando conectar ao MySQL: Tentativa $tentativa"
     tentativa=$((tentativa+1))
     if [ $tentativa -gt 10 ]; then
         echo "Não foi possível conectar ao MySQL após várias tentativas."
@@ -12,4 +11,7 @@ while ! mysql -h "localhost" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SELECT 1" 
     fi
 done
 
-echo "Conexão com MySQL bem-sucedida."
+# Agora, executa o comando de restauração.
+mysql -h $DB_HOST -u "$DB_USERNAME" -p"$DB_PASSWORD" "$DB_DATABASE" < /home/gta/server/mkbase.sql
+
+echo "Backup restaurado com sucesso."
