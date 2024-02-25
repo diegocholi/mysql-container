ac09b2573196e9289d93c236a641d506c8eecf80
Diego Choli Conceição <48794883+diegocholi@users.noreply.github.com>
2024-02-25 10:40:50 -0300

    Update database.sh

--- database.sh
+++ database.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Restaura Banco de Dados
 # Tentativa de conexão com o MySQL a cada 5 segundos, até 10 tentativas.
 tentativa=1
 while ! mysql -h "localhost" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SELECT 1" > /dev/null 2>&1; do
+    echo "Tentando conectar ao MySQL: Tentativa $tentativa"
     sleep 5
     tentativa=$((tentativa+1))
     if [ $tentativa -gt 10 ]; then
@@ -12,7 +12,4 @@ while ! mysql -h "localhost" -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SELECT 1" 
     fi
 done
 
-# Agora, executa o comando de restauração.
-mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE" < /path/to/your/sqlfile.sql
-
-echo "Backup restaurado com sucesso."
+echo "Conexão com MySQL bem-sucedida."
